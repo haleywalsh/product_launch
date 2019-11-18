024fb0a15ab689aa18c855e7c337d3280fd41fab
haleywalsh <haleywalsh@users.noreply.github.com>
2019-11-18 15:04:07 -0500

    haven't done this in awhile

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// smooth scroll
+
+$('a[href*="#"]').click(function(event){
+  
+  // prevent defalt click behavior
+  // don't jump to content
+  event.preventDefault();
+    
+  // identify position of target
+  var target = $(this).attr('href');
+  var top = $(target).offset().top;
+
+  // animate scroll to target
+  $('html,body').animate({
+    scrollTop: top
+    },500);
+
+});
+
+//

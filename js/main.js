922cbc56a3f353c536abf2fc66a19af0177d58e1
haleywalsh <haleywalsh@users.noreply.github.com>
2019-11-19 17:27:02 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+AOS.init();
+
+
 // smooth scroll
 
 $('a[href*="#"]').click(function(event){
@@ -18,3 +21,48 @@ $('a[href*="#"]').click(function(event){
 });
 
 //
+
+
+// faq
+
+ function pageLoad() {
+
+        $('#div1').hide();
+  }
+
+
+$(document).ready(function(){
+  $(".b1").click(function(){
+    $("#div1").fadeToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".b2").click(function(){
+    $("#div2").fadeToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".b3").click(function(){
+    $("#div3").fadeToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".b4").click(function(){
+    $("#div4").fadeToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".b5").click(function(){
+    $("#div5").fadeToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".b6").click(function(){
+    $("#div6").fadeToggle();
+  });
+});
